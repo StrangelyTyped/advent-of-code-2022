5f47ffdcc624bf1d94439a9df7dea6725fd5a517
Alexis A <strangelytyped@users.noreply.github.com>
2022-12-15 22:36:39 +0000

    Day 15, please dont judge

---
+++ day15/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"regexp"
+
+	"github.com/strangelytyped/advent-of-code-2022/utils"
+)
+
+const BEACON byte = 'B'
+const EMPTY byte = '#'
+const SENSOR byte = 'S'
+
+type Position [2]int
+
+type SensorResult struct {
+	beaconPos Position
+	delta     Position
+	distance  int
+}
+
+var parseRe = regexp.MustCompile(`-?\d+`)
+
+func distanceFromDelta(delta Position) int {
+	return delta[0] + delta[1]
+}
+
+func delta(d1, d2 Position) (Position, int) {
+	delta := Position{utils.Abs(d1[0] - d2[0]), utils.Abs(d1[1] - d2[1])}
+	return delta, distanceFromDelta(delta)
+}
+
+func Part1(input io.Reader, targetY int) int {
+	sensors, minX, maxX := parseInput(input)
+
+	cave := mapAtY(sensors, targetY)
+
+	emptyLocs := 0
+
+	for x := minX; x <= maxX; x++ {
+		spot := cave[x]
+
+		if spot == EMPTY {
+			emptyLocs++
+		}
+	}
+
+	return emptyLocs
+}
+
+func mapAtY(sensors map[Position]SensorResult, y int) map[int]byte {
+	cave := map[int]byte{}
+	for sensor, beaconData := range sensors {
+		if beaconData.beaconPos[1] == y {
+			cave[beaconData.beaconPos[0]] = BEACON
+		}
+		if sensor[1] == y {
+			cave[sensor[0]] = SENSOR
+		}
+		distanceLimit := beaconData.distance - (utils.Abs(y - sensor[1]))
+		if distanceLimit < 0 {
+			continue
+		}
+
+		for x := -distanceLimit; x <= distanceLimit; x++ {
+			if _, isMarked := cave[sensor[0] + x]; !isMarked {
+				cave[sensor[0] + x] = EMPTY
+			}
+		}			
+	}
+	return cave
+}
+
+
+func parseInput(input io.Reader) (map[Position]SensorResult, int, int) {
+	scanner := bufio.NewScanner(input)
+
+	sensors := map[Position]SensorResult{}
+	minX := math.MaxInt
+	maxX := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := parseRe.FindAllString(line, -1)
+		sensor := Position{utils.AtoiOrPanic(parts[0]), utils.AtoiOrPanic(parts[1])}
+		beacon := Position{utils.AtoiOrPanic(parts[2]), utils.AtoiOrPanic(parts[3])}
+		delta, distance := delta(sensor, beacon)
+		sensors[sensor] = SensorResult{beacon, delta, distance}
+
+		minX = utils.Min(sensor[0]-distance, minX)
+		maxX = utils.Max(sensor[0]+distance, maxX)
+	}
+	return sensors, minX, maxX
+}
+
+func check(sensors map[Position]SensorResult, x, y, coordLimit int) int {
+	if x < 0 || y < 0 || x > coordLimit || y > coordLimit {
+		return 0
+	}
+	for sensor, result := range sensors {
+
+		_, distance := delta(sensor, Position{x, y})
+		
+		if distance <= result.distance {
+			return 0
+		}
+	}
+	fmt.Printf("Found %d/%d\n", x, y)
+	return x * 4000000 + y
+}
+
+func Part2(input io.Reader, coordLimit int) int {
+
+	sensors, _, _ := parseInput(input)
+
+	for sensor, beaconData := range sensors { 
+		distance := beaconData.distance + 1
+		for x := -distance; x <= distance; x++ {
+			y1 := distance - x
+			y2 := -y1
+
+			if r := check(sensors, sensor[0] + x, sensor[1] + y1, coordLimit); r != 0 {
+				return r
+			}
+			if r := check(sensors, sensor[0] + x, sensor[1] + y2, coordLimit); r != 0 {
+				return r
+			}
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	utils.Run("day15.txt", func(r io.Reader) int { return Part1(r, 2000000) }, func(r io.Reader) int {return Part2(r, 4000000)})
+}
---
+++ day15/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/strangelytyped/advent-of-code-2022/utils"
+)
+
+const testInput string = `
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+`
+
+
+
+
+const testResultPart1 = 26
+const testResultPart2 = 56000011
+
+
+func TestPart1(t *testing.T) {
+	result := Part1(strings.NewReader(utils.CleanInput(testInput)), 10)
+	if result != testResultPart1 {
+		t.Errorf("expected %v, got %v", testResultPart1, result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(strings.NewReader(utils.CleanInput(testInput)), 20)
+	if result != testResultPart2 {
+		t.Errorf("expected %v, got %v", testResultPart2, result)
+	}
+}
